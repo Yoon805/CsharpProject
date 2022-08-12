ca8cdf4186f08a5233308f76467abf6ee68ba230
Yoon805 <yoond395@gmail.com>
2022-08-12 14:18:36 +0900

    Tetris 만들기 시작

---
+++ TetrisGame/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TetrisGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.gameTableScreen = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 189F));
+            this.tableLayoutPanel1.Controls.Add(this.metroButton1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.gameTableScreen, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 60);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 500F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 565);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(3, 523);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "startBtn";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // gameTableScreen
+            // 
+            this.gameTableScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gameTableScreen.Location = new System.Drawing.Point(172, 23);
+            this.gameTableScreen.Name = "gameTableScreen";
+            this.gameTableScreen.Size = new System.Drawing.Size(294, 494);
+            this.gameTableScreen.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 645);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.Panel gameTableScreen;
+    }
+}
+
---
+++ TetrisGame/Tetris.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TetrisGame
+{
+    internal class Tetris
+    {
+        public const int Width = 10;
+        public const int Height = 24;
+
+        List<int>[,] gameTable = new List<int>[10,20];
+        int blockSize = 20;
+
+
+        public void FirstDrawTable(Panel form) {
+
+            float width = Width * blockSize;
+            float height = Height * blockSize;
+
+            float x1 = 0;
+            float y1 = 0;
+
+            Graphics g;
+
+            try
+            {
+                g = form.CreateGraphics();
+            }
+            catch
+            {
+                return;
+            }
+
+            //SolidBrush myBrush = new SolidBrush(Color.Black);
+            Pen myPen = new Pen(Color.Black, 1);    
+
+            for (int i = 0; i < gameTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameTable.GetLength(1); j++)
+                {
+                    g.DrawRectangle(myPen, x1+i*blockSize, y1+j*blockSize, blockSize, blockSize);
+                }
+            };
+        }
+
+        private int[,] GetBlockArray(int block, int direction)
+        {
+            switch (block)
+            {
+                case 0:
+                    // ##
+                    // ##
+                    switch (direction)
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                            return new int[2, 2] { { 1, 1 }, { 1, 1 } };
+                    }
+                    break;
+                case 1:
+                    // ####
+                    switch (direction)
+                    {
+                        case 0:
+                        case 2:
+                            return new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+                        case 1:
+                        case 3:
+                            return new int[4, 4] { { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 } };
+                    }
+                    break;
+                case 2:
+                    //  ##
+                    // ##
+                    switch (direction)
+                    {
+                        case 0:
+                        case 2:
+                            return new int[3, 3] { { 0, 0, 0 }, { 0, 1, 1 }, { 1, 1, 0 } };
+                        case 1:
+                        case 3:
+                            return new int[3, 3] { { 0, 1, 0 }, { 0, 1, 1 }, { 0, 0, 1 } };
+                    }
+                    break;
+                case 3:
+                    // ##
+                    //  ##
+                    switch (direction)
+                    {
+                        case 0:
+                        case 2:
+                            return new int[3, 3] { { 0, 0, 0 }, { 1, 1, 0 }, { 0, 1, 1 } };
+                        case 1:
+                        case 3:
+                            return new int[3, 3] { { 0, 0, 1 }, { 0, 1, 1 }, { 0, 1, 0 } };
+                    }
+                    break;
+                case 4:
+                    // ###
+                    // #
+                    switch (direction)
+                    {
+                        case 0:
+                            return new int[3, 3] { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0 } };
+                        case 1:
+                            return new int[3, 3] { { 0, 1, 0 }, { 0, 1, 0 }, { 0, 1, 1 } };
+                        case 2:
+                            return new int[3, 3] { { 0, 0, 1 }, { 1, 1, 1 }, { 0, 0, 0 } };
+                        case 3:
+                            return new int[3, 3] { { 1, 1, 0 }, { 0, 1, 0 }, { 0, 1, 0 } };
+                    }
+                    break;
+                case 5:
+                    // ###
+                    //   #
+                    switch (direction)
+                    {
+                        case 0:
+                            return new int[3, 3] { { 0, 0, 0 }, { 1, 1, 1 }, { 0, 0, 1 } };
+                        case 1:
+                            return new int[3, 3] { { 0, 1, 1 }, { 0, 1, 0 }, { 0, 1, 0 } };
+                        case 2:
+                            return new int[3, 3] { { 1, 0, 0 }, { 1, 1, 1 }, { 0, 0, 0 } };
+                        case 3:
+                            return new int[3, 3] { { 0, 0, 1 }, { 0, 0, 1 }, { 0, 1, 1 } };
+                    }
+                    break;
+                case 6:
+                    // ###
+                    //  #
+                    switch (direction)
+                    {
+                        case 0:
+                            return new int[3, 3] { { 0, 0, 0 }, { 1, 1, 1 }, { 0, 1, 0 } };
+                        case 1:
+                            return new int[3, 3] { { 0, 1, 0 }, { 0, 1, 1 }, { 0, 1, 0 } };
+                        case 2:
+                            return new int[3, 3] { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 0, 0 } };
+                        case 3:
+                            return new int[3, 3] { { 0, 1, 0 }, { 1, 1, 0 }, { 0, 1, 0 } };
+                    }
+                    break;
+            }
+
+            return null;
+        }
+
+
+    }
+}
